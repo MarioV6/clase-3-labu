8ab128044bdb119a8d24f54952bac7b07b80bb43
MarioV6 <votesarein@gmail.com>
2026-02-25 21:30:00 -0400

    giro y nombre

--- src/app/components/avatar/avatar.ts
+++ src/app/components/avatar/avatar.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-avatar',
@@ -9,25 +9,31 @@ import { Component, signal } from '@angular/core';
   styleUrl: './avatar.scss',
 })
 export class AvatarComponent {
-  colorFondo = signal<string>('#D91818')
-  tamano = signal<number>(150)
-  tieneLentes = signal<boolean>(false)
-  tipoExpresion = signal<number>(1)
-
-  cambiarTamano(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.tamano.set(parseInt(input.value));
-  }
-  cambiarColor(event: Event) {
+ colorFondo = signal<string>('#3498db');
+ tamano = signal<number>(150);
+ tieneLentes = signal<boolean>(false);
+ tipoExpresion = signal<number>(1);
+ girar =signal<boolean>(false);
+ Cnombre = signal<string>('Nombechito');
+  cambiarColor(event: Event){
     const input = event.target as HTMLInputElement;
     this.colorFondo.set(input.value);
-    
   }
-
-  toggleLentes() {
+ cambiarTamano(event: Event){
+   const input = event.target as HTMLInputElement;
+   this.tamano.set(parseInt(input.value));
+ }
+ toggleLentes(){
     this.tieneLentes.update(flag => !flag);
-  }
-  cambiarExpresion(tipo: number) {
+ }
+ setExpresion(tipo: number){
     this.tipoExpresion.set(tipo);
-  }
+ }
+  toggleGirar(){
+    this.girar.update(flag => !flag);
+ }
+ setNombre(event: Event){
+  const input = event.target as HTMLInputElement;
+  this.Cnombre.set(input.value);
+}
 }
